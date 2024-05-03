adcc1fffa436ff27e47c47ee85551ca0f92366ba
kdurkin77 <krissy7791@yahoo.com>
2024-05-03 14:08:49 -0400

    removing non net standard conditionals since we no longer have legacy framework direct support
    Change-Id: Idf2bd9bc8196d2ac7a6c222e4f4f13450ed2fb00

--- DTLS.Net/Client.cs
+++ DTLS.Net/Client.cs
@@ -89,13 +89,8 @@ namespace DTLS
         public List<TCipherSuite> SupportedCipherSuites { get; }
         public byte[] ServerCertificate { get; set; }
 
-#if NETSTANDARD2_1 || NETSTANDARD2_0
         private CngKey _PrivateKeyRsa;
         public CngKey PublicKey { get; set; }
-#else
-        private RSACryptoServiceProvider _PrivateKeyRsa;
-        public RSACryptoServiceProvider PublicKey { get; set; }
-#endif
 
         public Client(EndPoint localEndPoint)
             : this(localEndPoint, [])
@@ -971,6 +966,7 @@ namespace DTLS
             }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Other methods are available but RSA is just for windows")]
         public void LoadX509Certificate(X509Chain chain)
         {
             if (chain == null)
@@ -980,17 +976,12 @@ namespace DTLS
 
             var mainCert = chain.ChainElements[0].Certificate;
 
-#if NETSTANDARD2_1 || NETSTANDARD2_0
+#pragma warning disable SYSLIB0028 // Type or member is obsolete
             _PrivateKeyRsa = ((RSACng)mainCert.PrivateKey).Key;
+#pragma warning restore SYSLIB0028 // Type or member is obsolete
+#pragma warning disable SYSLIB0027 // Type or member is obsolete
             PublicKey = ((RSACng)mainCert.PublicKey.Key).Key;
-#else
-#pragma warning disable SYSLIB0028 // Type or member is obsolete - haven't been able to find a way without exporting the private key
-            _PrivateKeyRsa = (RSACryptoServiceProvider)mainCert.PrivateKey;
-#pragma warning restore SYSLIB0028 // Type or member is obsolete - haven't been able to find a way without exporting the private key
-#pragma warning disable SYSLIB0027 // Type or member is obsolete - haven't been able to find a way without exporting the private key
-            PublicKey = (RSACryptoServiceProvider)mainCert.PublicKey.Key;
-#pragma warning restore SYSLIB0027 // Type or member is obsolete - haven't been able to find a way without exporting the private key
-#endif
+#pragma warning restore SYSLIB0027 // Type or member is obsolete
 
             var certChain = new List<byte[]>();
             foreach (var element in chain.ChainElements)
